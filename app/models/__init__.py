6e9b835058baebdda2441f5232566d4257388acf
Gefferson Casasola <gcasasolah@uni.pe>
2025-03-16 19:11:23 -0500

    (BPO-8) feat: refactor models file names for better structure

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,5 @@ from app.models.patient import Patient
 from app.models.doctor import Doctor
 from app.models.appointment import Appointment
 from app.models.schedule import Schedule
-from app.models.consultingroom import ConsultingRoom
-from app.models.doctoroom import DoctorRoom
+from app.models.consulting_room import ConsultingRoom
+from app.models.doctor_room import DoctorRoom
